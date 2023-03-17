d09a2c4d242d069d17eb8a24a15d10e1a05ffaa6
pygoscelis02 <106791137+pygoscelis02@users.noreply.github.com>
2023-03-17 14:52:32 +0900

    20230317：unwantedActivityInJS-CommentOut

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+
+
+// //時間帯で画像の表示切り替え
+// var now = new Date();
+// var hour = now.getHours();
+
+// //img_element_pc要素を作成
+// let img_element_pc = document.createElement(`img`);
+
+// if(hour >= 0 && hour <= 5){
+//     img_element_pc.src = 'images/key4-1.jpg';
+// }
+// if(hour >= 6 && hour <= 16){
+//     img_element_pc.src = 'images/key1-1.jpg';
+// }
+// if(hour >= 17 && hour <= 19){
+//     img_element_pc.src = 'images/key2-1.jpg';
+// }
+// if(hour >= 20 && hour <= 23){
+//     img_element_pc.src = 'images/key3-1.jpg';
+// }
+
+// let img_pc = document.getElementsByClassName("img-pc");
+// img_pc.appendChild(img_element_pc);
+
+
+
+// //img_element_pc要素を作成
+// let img_element_tab = document.createElement(`img`);
+
+// if(hour >= 0 && hour <= 5){
+//     img_element_tab.src = "images/key4-2.jpg";
+// }
+// if(hour >= 6 && hour <= 16){
+//     img_element_tab.src = "images/key1-2.jpg";
+// }
+// if(hour >= 17 && hour <= 19){
+//     img_element_tab.src = "images/key2-2.jpg";
+// }
+// if(hour >= 20 && hour <= 23){
+//     img_element_tab.src = "images/key3-2.jpg";
+// }
+
+// var img_tab = document.getElementsByClassName("img-tab");
+// img_tab.appendChild(img_element_tab);
+
