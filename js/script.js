c09926ad4f19453a4dad8833cb575ab982046669
pygoscelis02 <106791137+pygoscelis02@users.noreply.github.com>
2023-03-19 18:55:14 +0900

    20230319-3

--- js/script.js
+++ js/script.js
@@ -10,15 +10,15 @@ let img_element_pc = document.createElement('img');
 img_element_pc.id = "img-pc";
 
 if(hour >= 0 && hour <= 5){
-    img_element_pc.src = 'images/key4-1.jpg';
+    img_element_pc.src = 'images/key3-1.jpg';
 }
-if(hour >= 6 && hour <= 16){
+else if(hour >= 6 && hour <= 16){
     img_element_pc.src = 'images/key1-1.jpg';
 }
-if(hour >= 17 && hour <= 19){
+else if(hour >= 17 && hour <= 19){
     img_element_pc.src = 'images/key2-1.jpg';
 }
-if(hour >= 20 && hour <= 23){
+else if(hour >= 20 && hour <= 23){
     img_element_pc.src = 'images/key3-1.jpg';
 }
 
@@ -33,15 +33,15 @@ let img_element_tab = document.createElement('img');
 img_element_tab.id = "img-tab";
 
 if(hour >= 0 && hour <= 5){
-    img_element_tab.src = "images/key4-2.jpg";
+    img_element_tab.src = "images/key3-2.jpg";
 }
-if(hour >= 6 && hour <= 16){
+else if(hour >= 6 && hour <= 16){
     img_element_tab.src = "images/key1-2.jpg";
 }
-if(hour >= 17 && hour <= 19){
+else if(hour >= 17 && hour <= 19){
     img_element_tab.src = "images/key2-2.jpg";
 }
-if(hour >= 20 && hour <= 23){
+else if(hour >= 20 && hour <= 23){
     img_element_tab.src = "images/key3-2.jpg";
 }
 
